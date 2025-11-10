ae4b41b3d7c4c8d596e1dc4e155ca2ff65e1fe45
Abdul Ahad <abdulahad43149031@gmail.com>
2025-11-10 13:47:43 +0530

    Added freq of word in a sentence program

---
+++ frequencyOfWord.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+void countFrequency(){
+    char sentence[100];
+    char temp[20];
+    char word[20];
+    int occurences = 0;
+
+    printf("Enter the string:\n");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    // remove trailing '\n' from sentence
+    int i = 0;
+    while(sentence[i] != '\0'){
+        if(sentence[i] == '\n') sentence[i] = '\0';
+        i++;
+    }
+    i = 0;
+
+    printf("Enter the word:\n");
+    fgets(word, sizeof(word), stdin);
+
+    // remove trailing '\n' from word
+    int j = 0;
+    while(word[j] != '\0'){
+        if(word[j] == '\n') word[j] = '\0';
+        j++;
+    }
+
+    while(sentence[i] != '\0'){
+        // extract word
+        j = 0;
+        while(sentence[i] != ' ' && sentence[i] != '\0'){
+            temp[j++] = sentence[i++];
+        }
+        temp[j] = '\0'; // null terminate temp
+
+        // compare temp and word manuallu
+        int k = 0, flag = 1;
+        while(temp[k] != '\0' || word[k] != '\0'){
+            if(temp[k] != word[k]){
+                flag = 0;
+            }
+            k++;
+        }
+        if(flag == 1){
+            occurences++;
+        }
+
+        while(sentence[i] == ' ') i++; // skip spaces
+    }
+
+    printf("Number of occurences of word: %d", occurences);
+}
+
+int main(){
+    countFrequency();
+}
